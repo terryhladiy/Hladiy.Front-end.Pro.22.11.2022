6b0fd8edb696555c863d167e26b3e406c83dd847
taras <tarik2002sta@gmail/com>
2023-01-10 01:50:20 +0200

    HW 14#2

--- Hillel/HW 14/script.js
+++ Hillel/HW 14/script.js
@@ -11,3 +11,35 @@ function numbers(newArray) {
   return (sum / sumLength.length);
 }
 numbers([5, 66, 'pool', 7, 99, null, { name: 'Sam' }, [], true, 78]);
+// #2
+
+function doMath(x, znak, y) {
+  let result = 0;
+  switch (znak) {
+    case '+':
+      result = (x + y);
+      break;
+    case '-':
+      result = (x - y);
+      break;
+    case '*':
+      result = (x * y);
+      break;
+    case '/':
+      result = (x / y);
+      break;
+    case '%':
+      result = (x * y) / 100;
+      break;
+    case '^':
+      result = (x ** y);
+      break;
+    default:
+      result = ('Ooops... Щось пішло не так');
+  }
+  return result;
+}
+const xNum = +prompt('Введіть перше чило');
+const yNum = +prompt('Ведіть друге число');
+const zNum = prompt('Введіть що ви хочете зробити', ' +, -, *, /, %, ^');
+alert(doMath(xNum, zNum, yNum));
