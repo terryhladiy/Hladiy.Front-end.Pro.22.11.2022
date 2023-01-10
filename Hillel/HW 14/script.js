dcc49fa37c4ad3f26ad09d484c48baef42ae6510
taras <tarik2002sta@gmail/com>
2023-01-10 02:03:33 +0200

    HW 14 #3

--- Hillel/HW 14/script.js
+++ Hillel/HW 14/script.js
@@ -43,3 +43,17 @@ const xNum = +prompt('Введіть перше чило');
 const yNum = +prompt('Ведіть друге число');
 const zNum = prompt('Введіть що ви хочете зробити', ' +, -, *, /, %, ^');
 alert(doMath(xNum, zNum, yNum));
+// #3
+const inCount = +prompt('Введіть довжину масиву');
+const onCount = +prompt('Введіть довжину підмасивів');
+function makeArray(lengthXarr, lengthYarr) {
+  const arr = [];
+  for (let i = 0; i < lengthXarr; i += 1) {
+    arr.push([]);
+    for (let j = 0; j < lengthYarr; j += 1) {
+      arr[i].push(prompt(`Ведіть elemnt номер ${j} з ${lengthYarr} `));
+    }
+  }
+  return arr;
+}
+console.log(makeArray(inCount, onCount));
